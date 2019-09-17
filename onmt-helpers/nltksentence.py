de497f6c0e566d2e30895a540129a2b67c444e03
jenec2 <jen@sevenminuteserver.com>
2019-09-17 20:19:53 +0000

    Add an option (-l) to remove sentences smaller than n words

--- onmt-helpers/nltksentence.py
+++ onmt-helpers/nltksentence.py
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 
 # Uses nltk to break a file into sentences
-# Usage: nltk-sentences.py infile outfile
+# Usage: nltk-sentences.py -s srcfile -o outputfile [-p] [-l n] 
 
 import sys
 import string
 import re
 import argparse
+
+from nltk.tokenize import sent_tokenize
 
 if (sys.version_info > (3, 0)):
     pass
@@ -15,10 +17,11 @@ else:
     reload(sys)
     sys.setdefaultencoding('utf-8')
 
-parser = argparse.ArgumentParser(description='nltk-sentences.py: Use nltk to break a file into individual sentences')
+parser = argparse.ArgumentParser(description='nltk-sentences.py: Use nltk to break a file into individual sentences, optionally strip punctuation or remove short sentences')
 parser.add_argument('-s','--source',help='Source File', required=True)
 parser.add_argument('-o','--output',help='Output File', required=True)
 parser.add_argument('-p','--strip_punc',help='Strip punctuation',action='store_true',required=False)
+parser.add_argument('-l','--sentence_length',help='Remove sentences shorter than the specified amount',required=False)
 args = parser.parse_args()
 
 if (args.source):
@@ -33,6 +36,11 @@ if (args.strip_punc):
     strip_punc = True
 else:
     strip_punc = False
+if (args.sentence_length):
+    long = True
+    sentence_length = int(args.sentence_length)
+else:
+    long = False
 
 filename = srcfile
 
@@ -41,14 +49,23 @@ text = file.read()
 file.close()
 out = open(outfile,'w')
 # split into sentences
-from nltk import sent_tokenize
-sentences = sent_tokenize(text)
-for line in sentences:
+sentences = sent_tokenize(text) 
+for index in sentences:
+    index = index.replace('\n\n','\n').replace('\n\n','\n')
     if strip_punc == True:
        translate_table = dict((ord(char), " ") for char in string.punctuation)
-       line = line.translate(translate_table)
-       line = re.sub(' +', ' ', line)
+       index = index.translate(translate_table)
+       index = re.sub(' +', ' ', index)
     else:
         pass
-    out.write(line)
-    out.write("\n")
+    if long == True:
+        words = index.split() 
+        word_count = len(words)
+        if word_count > sentence_length:
+            out.write(index)
+            out.write("\n")
+        else:
+            pass
+    else:
+        out.write(index)
+        out.write("\n")
